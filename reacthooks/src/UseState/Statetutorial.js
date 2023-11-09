6df5ed2e15ae8ad2cf8c92c27814038e42bc08e3
OKORIE DANIEL EZICHI <zichdan1999@gmail.com>
2023-11-09 14:10:52 +0000

    Done with UseState

--- reacthooks/src/UseState/Statetutorial.js
+++ reacthooks/src/UseState/Statetutorial.js
@@ -1,19 +1,62 @@
-import React, { useState } from "react";
+import React, { useState } from 'react'
 
-const StateTutorial = () => {
-  const [inputValue, setInputValue] = useState("Pedro");
+const Statetutorial = () => {
+  const [inputvalue, setInputValue] = useState("Hi! Daniel");
 
-  let onChange = (event) => {
-    const newValue = event.target.value;
-    setInputValue(newValue);
-  };
+
+  let onchange = (event) => {
+    const newvalue = event.target.value;
+    setInputValue(newvalue);
+  }
 
   return (
-    <div>
-      <input placeholder="enter something..." onChange={onChange} />
-      {inputValue}
-    </div>
-  );
-};
 
-export default StateTutorial;
+    <div>
+      <input type="text" placeholder='Please input something ........' onChange={onchange} />
+    {inputvalue}
+    </div>
+  )
+}
+
+export default Statetutorial
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React, { useState } from "react";
+
+// const StateTutorial = () => {
+//   const [inputValue, setInputValue] = useState("Pedro");
+
+//   let onChange = (event) => {
+//     const newValue = event.target.value;
+//     setInputValue(newValue);
+//   };
+
+//   return (
+//     <div>
+//       <input placeholder="enter something..." onChange={onChange} />
+//       {inputValue}
+//     </div>
+//   );
+// };
+
+// export default StateTutorial;
