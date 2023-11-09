d18337fff6ff0dee4cbeb7fe07b0d0066d49d75f
OKORIE DANIEL EZICHI <zichdan1999@gmail.com>
2023-11-09 13:14:16 +0000

    Changed the tool to React instead of Nextjs

---
+++ reacthooks/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import StateTutorial from './UseState/Statetutorial';
+
+function App() {
+  return (
+    <div className="App">
+      <StateTutorial/>
+    </div>
+  );
+}
+
+export default App;
