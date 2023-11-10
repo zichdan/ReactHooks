62311dc0a68e365eb7364b9c0f5bd9732bf2868c
OKORIE DANIEL EZICHI <zichdan1999@gmail.com>
2023-11-10 15:09:10 +0000

    unknown

--- reacthooks/src/UseReducer/ReducerTutorial.js
+++ reacthooks/src/UseReducer/ReducerTutorial.js
@@ -1,22 +1,62 @@
-import React, { useState } from 'react'
+import React, { useReducer } from 'react'
 
-const ReducerTutorial = () => {
-    const [count, setCount] = useState(0);
-    const [showText, setshowText] = useState(true);
 
-  return (
-    <div>
-        <h1>{count}</h1>
-        <button onClick={() => {
-            setCount(count + 1)
-            setshowText(!showText)
-        }}
-        >
-        Click Here 
-        </button>
-        {showText &&  <p>This is the Text</p> }
-    </div>
-  )
+
+const reducer = (state, action ) => {
+  switch (action.type) {
+    
+  }
+
+
+
+  switch (key) {
+    case value:
+      
+      break;
+  
+    default:
+      break;
+  }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// //  Example of use reducer using "UseState Props"
+
+// const ReducerTutorial = () => {
+//     const [count, setCount] = useState(0);
+//     const [showText, setshowText] = useState(true);
+
+//   return (
+//     <div>
+//         <h1>{count}</h1>
+//         <button onClick={() => {
+//             setCount(count + 1)
+//             setshowText(!showText)
+//         }}
+//         >
+//         Click Here 
+//         </button>
+//         {showText &&  <p>This is the Text</p> }
+//     </div>
+//   )
+// }
 
 export default ReducerTutorial
